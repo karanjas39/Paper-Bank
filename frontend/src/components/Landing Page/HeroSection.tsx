7f88b67af4a218b9e08d3521ca1163d878e34b2d
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-09-16 04:45:52 +0530

    fixes the style of hero section

--- frontend/src/components/Landing Page/HeroSection.tsx
+++ frontend/src/components/Landing Page/HeroSection.tsx
@@ -53,7 +53,7 @@ function HeroSection() {
   const words = ["faster", "easier", "smarter"];
 
   return (
-    <div className="w-full px-7 h-screen dark:bg-background bg-background  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+    <div className="w-full px-7 h-screen dark:bg-background bg-background  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex md:items-center justify-center items-start">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-background bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="text-5xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 flex flex-col gap-2 text-wrap">
         <div className="text-5xl sm:text-7xl font-light text-neutral-600 dark:text-neutral-400 flex flex-col gap-4">
