62600e73564f292bad21d779cda68ca3f2705b27
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-09-27 15:09:41 +0530

    new update

--- frontend/src/components/Providers/RootProvider.tsx
+++ frontend/src/components/Providers/RootProvider.tsx
@@ -7,14 +7,12 @@ import { store } from "@/store/index";
 import { Provider } from "react-redux";
 import { Toaster } from "@/components/ui/toaster";
 import { usePathname } from "next/navigation";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const RootProvider = ({ children }: { children: React.ReactNode }) => {
   const path = usePathname();
 
   return (
     <Provider store={store}>
-      <SpeedInsights />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
