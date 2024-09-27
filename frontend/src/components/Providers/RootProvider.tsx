a429814cb46616e06a35cc89943845cd1c03bca6
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-09-27 15:03:42 +0530

    added speed insights in frontend

--- frontend/src/components/Providers/RootProvider.tsx
+++ frontend/src/components/Providers/RootProvider.tsx
@@ -7,12 +7,14 @@ import { store } from "@/store/index";
 import { Provider } from "react-redux";
 import { Toaster } from "@/components/ui/toaster";
 import { usePathname } from "next/navigation";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const RootProvider = ({ children }: { children: React.ReactNode }) => {
   const path = usePathname();
 
   return (
     <Provider store={store}>
+      <SpeedInsights />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
